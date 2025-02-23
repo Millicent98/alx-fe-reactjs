1ffb999fff8b4105ad55437e2e16bc681e9ee34e
Millicent Mafiri <mpilomillicent@gmail.com>
2025-02-23 18:05:55 +0200

    added more files and folders in src

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/footer";
+import UserProfile from "./components/UserProfile";
+
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+  );
+}
+
+export default App;
