ff49d1631760780fe542868253877ecc89d043eb
Millicent Mafiri <mpilomillicent@gmail.com>
2025-03-11 05:48:07 +0200

    addded welcome message,userprifile,header,foote and maincontent

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2> {/* Display the user's name */}
+      <p>Age: {props.age}</p> {/* Display the user's age */}
+      <p>Bio: {props.bio}</p> {/* Display the user's bio */}
+    </div>
+  );
+};
+
+export default UserProfile;
